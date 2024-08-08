20ca47e47dc339f8a2e603b2ec8321a3863c4003
Lucas Junio <lucas.verdine@gmail.com>
2024-08-08 10:13:19 -0300

    inital commit

---
+++ android/app/src/main/kotlin/com/hello/world/hello_world/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hello.world.hello_world
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/splash.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, 
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 45),
+              child: Text('Hello World!'),
+            ), 
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
